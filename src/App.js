63b4ea20fd5c009dd2cbeca57da2efb397f76414
valnuccio <val.nuccio@gmail.com>
2020-10-14 10:38:52 -0400

    everything but hiding form

--- src/App.js
+++ src/App.js
@@ -33,13 +33,44 @@ class App extends React.Component {
    let poems = data.map(poem =>{return{...poem, read:false}})
    this.setState({poems:poems})
  }
+
+ postIt=(poem)=>{
+
+
+
+   let API= "http://localhost:6001/poems/"
+   console.log('im here,', poem)
+
+   let options={
+     method:'POST',
+     headers: {
+       'content-type':'application/json',
+       'accepts': 'application/json'
+     },
+     body: JSON.stringify(poem)
+   }
+
+
+
+   fetch(API, options)
+   .then(res=>res.json())
+   .then(res=>this.addNewPoemToState(res))
+
+   
+ }
+
+ addNewPoemToState = (poem) =>{
+    let newArray = [...this.state.poems, poem]
+    this.setState({poems:newArray})
+ }
+
   render() {
     console.log(this.state.poems)
     return (
       <div className="app">
         <div className="sidebar">
           <button onClick={this.openForm}>Show/hide new poem form</button>
-          {this.state.formOpen? <NewPoemForm /> : null}
+          {this.state.formOpen? <NewPoemForm postIt={this.postIt}/> : null}
         </div>
         <PoemsContainer poems={this.state.poems} clickHandler={this.clickHandler}/>
       </div>
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,12 +1,31 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+
+  state={
+    title:"",
+    author:"",
+    content:""
+  }
+
+  upDateState =(e)=>{
+    this.setState({[e.target.name]:[e.target.value]})
+    
+  }
+
+  wrangleData=(e)=>{
+    e.preventDefault();
+    this.props.postIt(this.state)
+    this.setState({title:'',author:'', content:''})
+  }
+
+
   render() {
     return (
-      <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
+      <form className="new-poem-form" onSubmit={this.wrangleData}>
+        <input placeholder="Title" name="title" value={this.state.title} onChange={this.upDateState}/>
+        <input placeholder="Author" name="author" value={this.state.author} onChange={this.upDateState}/>
+        <textarea placeholder="Write your masterpiece here..." rows={10} name="content" value={this.state.content} onChange={this.upDateState}/>
         <input type="submit" value="Share your masterpiece" />
       </form>
     );
