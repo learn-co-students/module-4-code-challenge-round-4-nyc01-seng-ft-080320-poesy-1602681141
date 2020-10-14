ecb9c5d53405a53c4eba70dc3e4eb70fd25c63c8
royFloresNyc <royfloresnyc@gmail.com>
2020-10-14 09:50:22 -0400

    Show/hide form to create new poems

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+    state = {
+        formButtonClicked: false
+    }
+
+    formButtonClickHandler = () => {
+        this.setState(prevState => { 
+            return { formButtonClicked: !prevState.formButtonClicked } 
+        })
+    }
+
+    render() {
+        return (
+        <div className="app">
+            <div className="sidebar">
+            <button onClick={this.formButtonClickHandler}>Show/hide new poem form</button>
+            {this.state.formButtonClicked ? <NewPoemForm /> : null}
+            </div>
+            <PoemsContainer />
         </div>
-        <PoemsContainer />
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
