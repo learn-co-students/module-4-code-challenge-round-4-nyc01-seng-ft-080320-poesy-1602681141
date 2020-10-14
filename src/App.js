236402b2233770b968af069f2ca4c56dd52cb9ee
alerner1 <alyssplerner@gmail.com>
2020-10-14 09:36:42 -0400

    deliverable 2 done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+  state = {
+    showForm: false
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({showForm: !prevState.showForm}))
+  }
+
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={this.handleClick}>Show/hide new poem form</button>
+          {this.state.showForm && <NewPoemForm />}
         </div>
         <PoemsContainer />
       </div>
