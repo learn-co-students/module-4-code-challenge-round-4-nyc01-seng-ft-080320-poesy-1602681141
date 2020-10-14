08a495bddd15e68e0f0aacefb92bd75e91fe27c2
alerner1 <alyssplerner@gmail.com>
2020-10-14 09:34:41 -0400

    deliverable 1 done

--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -2,11 +2,25 @@ import React from "react";
 import Poem from "./Poem";
 
 class PoemsContainer extends React.Component {
+  state = {
+    poems: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:6001/poems')
+      .then(resp => resp.json())
+      .then(poems => this.setState({poems: poems}))
+  }
+
+  mapPoems = () => {
+    return this.state.poems.map(poem => { return <Poem poem={poem} key={poem.id} /> })
+  }
+
   render() {
     return (
       <div className="poems-container">
         {
-          // render poems here
+          this.mapPoems()
         }
       </div>
     );
