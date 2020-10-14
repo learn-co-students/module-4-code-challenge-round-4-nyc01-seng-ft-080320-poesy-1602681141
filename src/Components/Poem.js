56a9da3f077113a642c9310615d0f3368a5930fb
anickel101 <anson.nickel@gmail.com>
2020-10-14 10:20:56 -0400

    4/4 Basic Deliverables

--- src/Components/Poem.js
+++ src/Components/Poem.js
@@ -2,13 +2,29 @@ import React from 'react'
 
 class Poem extends React.Component {
 
+    state={
+        read: false
+    }
+
+    readPoem = () => {
+        this.setState(prevState => {
+            return {
+                read: !prevState.read
+            }
+        })
+    }
+
+    buttonText = () => {
+        return this.state.read ? "Mark as Unread" : "Mark as Read"
+    }
+
     render() {
         return (
             <div>
                 <h4>{this.props.poem.title}</h4>
                 {this.props.poem.content}
                 <h4>By - {this.props.poem.author}</h4>
-                <button>Mark as Read</button>
+                <button onClick={this.readPoem}>{this.buttonText()}</button>
             </div>
         )
     }
