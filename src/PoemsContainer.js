882116ed0c3f2304a653c178829b89a2c0793fda
royFloresNyc <royfloresnyc@gmail.com>
2020-10-14 09:44:09 -0400

    Render poems from API

--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -2,15 +2,30 @@ import React from "react";
 import Poem from "./Poem";
 
 class PoemsContainer extends React.Component {
-  render() {
-    return (
-      <div className="poems-container">
-        {
-          // render poems here
-        }
-      </div>
-    );
-  }
+    state = {
+        poemsList: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:6001/poems')
+            .then(resp => resp.json())
+            .then(poemsApi => {
+                this.setState( { poemsList: poemsApi})
+            })
+    }
+
+    renderPoems = () => {
+        return this.state.poemsList.map((poemObj, indx) => <Poem key={indx} poem={poemObj}/>)
+    }
+
+    render() {
+        console.log(this.state.poemsList)
+        return (
+        <div className="poems-container">
+            {this.renderPoems()}
+        </div>
+        );
+    }
 }
 
 export default PoemsContainer;
