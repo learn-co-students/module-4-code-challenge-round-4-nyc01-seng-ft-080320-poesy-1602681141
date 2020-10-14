770e905054018fb1e89fb728317bb287f14ff717
Mattie43 <ericksenm93@ymail.com>
2020-10-14 10:01:31 -0400

    4/4 cores done

--- src/Poem.js
+++ src/Poem.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 class Poem extends React.Component {
+  state = {
+    read: true
+  }
+
+  readClick = () => {
+    this.setState({read: !this.state.read})
+  }
+
   render() {
+    let read = 'read'
+    if(!this.state.read){read = 'unread'}
+
     return (
       <div>
         <h3>{this.props.poem.title}</h3>
@@ -9,7 +20,7 @@ class Poem extends React.Component {
         <p>
           <strong>- {this.props.poem.author}</strong>
         </p>
-        <button>Mark as read</button>
+        <button onClick={this.readClick}>Mark as {read}</button>
       </div>
     );
   }
