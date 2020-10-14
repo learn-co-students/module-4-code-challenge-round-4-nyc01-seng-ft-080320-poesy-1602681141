15410013eef585922086668ab35852896607d9c8
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-10-14 09:40:58 -0400

    rendered all poems and able to mark poem as read/unread

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+    state ={
+        api: []
+    }
+
+    componentDidMount = () => {
+        fetch('http://localhost:3000/poems')
+        .then(resp => resp.json())
+        .then(poems => {
+            this.setState({api: poems})
+        })
+    }
   render() {
     return (
       <div className="app">
@@ -11,7 +22,7 @@ class App extends React.Component {
           <button>Show/hide new poem form</button>
           {false && <NewPoemForm />}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poems={this.state.api}/>
       </div>
     );
   }
